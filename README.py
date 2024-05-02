72758e8943a0d3179aba8f22bfc35c8327652682
Pakbung Pimnara <142192211+pakbung2000@users.noreply.github.com>
2024-05-02 12:33:53 +0700

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,8 @@
+
+# Hello World
+greeting = {
+              name: "Pakbung",
+              education: ["English", "Computer Science"],
+              favorite: [1984, "Evangelion", "Arrival(2016)", "Under Pressure(1981) by Queen, David Bowie"]
+            }
+
