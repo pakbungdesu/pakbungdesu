10265d798e686b2796eb54505abc3562d59d7fc3
Pimnara Sujaritchat <142192211+pakbungdesu@users.noreply.github.com>
2024-07-10 11:31:56 +0700

    Update README.py

--- README.py
+++ README.py
@@ -1,9 +1,9 @@
 
 # Hi there 👋
 greeting = {
-              name: "Pakbung",
-              education: ["English", "Computer Science"],
-              favorite: [1984, "Evangelion", "Arrival(2016)", "Under Pressure(1981) by Queen, David Bowie"],
-              interest: ["Data structure and Algorithms", "Machine Learning"],
+              "name": "Pakbung",
+              "education": ["English and Linguistics", "Computer Science"],
+              "favorite": [1984, "Evangelion", "Arrival(2016)", "Under Pressure by Queen, David Bowie"],
+              "interest": ["Computer Linguistics", "Machine Learning"],
             }
 
