c72f3471d024f83d9e5d40cbb0391ee5cff3872e
Pimnara Sujaritchat <142192211+pakbungdesu@users.noreply.github.com>
2024-11-14 15:20:07 +0700

    Update README.py

--- README.py
+++ README.py
@@ -3,7 +3,7 @@
 greeting = {
               "name": ["Pakbung"],
               "education": ["English and Linguistics", "Computer Science"],
-              "favorite": ["Arrival(2016)", "Radiohead"],
+              "favorite": ["Fight Club (1999)", "Radiohead"],
               "interest": ["Data Structure and Algorithms", "Machine Learning"],
             }
 
